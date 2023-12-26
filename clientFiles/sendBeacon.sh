626ddbf1b18881462b12c8029085f18882425f45
Tim L:udtke <timluedtke@users.noreply.github.com>
2023-12-26 17:22:01 +0100

    reformatting sendBeacon.sh

--- clientFiles/sendBeacon.sh
+++ clientFiles/sendBeacon.sh
@@ -5,13 +5,12 @@ deviceName="DEVICENAME"
 ## CPU TEMPERATURE READOUT FOR >>RASPBERRY PIs<< !
 raspiTemp=$(vcgencmd measure_temp)
 cpuTemp=${raspiTemp:5:4}
-
-## CPU TEMPERATURE READOUT FOR >>linux computers (eg. ubuntu)<< !
+## CPU TEMPERATURE READOUT FOR >>linux computers<< !
 #cpuTemp=$(sensors | grep -oP 'Package id 0: .*?\+\K[0-9.]+')
 
 privateip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 theuptime=$(cat /proc/stat | grep btime | awk '{ print $2 }')
-payload="" ## if you wand to send additional data e.g.: $(python ~/Adafruit_Python_DHT/examples/dht22.py)
+payload="" ## if you want to send additional data or use values from external sensors, eg.: $(python ~/Adafruit_Python_DHT/examples/dht22.py)
 
 curl -G "https://yourserver.com/beacon/hey.php" \
     --data whois="$deviceName" \
